039b42b04852041efa5f397efc617f46d8a18cde
lemon857 <lemon857.an@gmail.com>
2026-06-27 01:52:19 +0500

    crit fix: change session token column name to hash_token

--- internal/storage/entity/session.go
+++ internal/storage/entity/session.go
@@ -9,7 +9,7 @@ import (
 type Session struct {
 	SessionUuid					uuid.UUID `gorm:"column:session_uuid;primaryKey;type:uuid"`
 	UserUuid						uuid.UUID `gorm:"column:user_uuid;type:uuid"`
-	HashToken						string 		`gorm:"column:token"`
+	HashToken						string 		`gorm:"column:hash_token"`
 	ExpiresAt						time.Time `gorm:"column:expires_at"`
 	IsRevoked						bool 			`gorm:"column:is_revoked"`
 }
